d44919fb008dbdaa4946eff79527cfc20a85f908
PhuongNQ <nquangphuong@gmail.com>
2014-01-04 13:09:20 +0900

    add home page after logged in

--- presentice-web/public/js/index.js
+++ presentice-web/public/js/index.js
@@ -2,16 +2,6 @@ $( document ).ready(function() {
   Parse.initialize("Q7ub1yg5A0AmDAnwnzVc2SS0X0Q4UZMefq3Kukdf", "dpqGmdW8VDtxsBwJ93hOWQqekd9M4OQwc8flxhmI");
   //check user login
   if(Parse.User.current()){
-  	$("#login").html("<a href='/logout'>Logout</a>");
-  } else {
-  	$("#login").html("<a href='/login'>Login</a>");
+  	window.location.href = "/home";
   }
-
-  //logout
-  $("#login a").click(function(){
-  	if(Parse.User.current()){
-  		Parse.User.logOut();
-  	}
-  });
-
 });
