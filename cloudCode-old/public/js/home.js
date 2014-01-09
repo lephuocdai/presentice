db77b79f4b760b1dc43a02f83b671d7e970029c7
PhuongNQ <nquangphuong@gmail.com>
2014-01-09 23:50:06 +0900

    delete cloudCode-old

--- cloudCode-old/public/js/home.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-   Parse.initialize("Q7ub1yg5A0AmDAnwnzVc2SS0X0Q4UZMefq3Kukdf", "dpqGmdW8VDtxsBwJ93hOWQqekd9M4OQwc8flxhmI");
- 	$("#logout a").click(function(){
-        if(Parse.User.current()){
-            Parse.User.logOut();
-        }
-	});
-});
