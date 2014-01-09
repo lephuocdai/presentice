521313c822811ca85b2dbe875e1a87a0615ac36b
PhuongNQ <nquangphuong@gmail.com>
2014-01-10 01:43:29 +0900

    add cloud code to push notification

--- cloud-code/cloud/main.js
+++ cloud-code/cloud/main.js
@@ -1,7 +1,29 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-require('cloud/app.js');
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("hello, welcome to Presentice!");
+Parse.Cloud.afterSave("Review", function(request) {
+	var fromUser = request.object.get("fromUser");
+	var targetVideo = request.object.get("targetVideo");
+	var videoObj = Parse.Object.extend("Video");
+	var query = new Parse.Query(videoObj);
+	query.get(targetVideo.id, {
+		success: function(video){
+			var toUser = video.get("user");
+			Parse.Push.send({
+  				channels: [ fromUser.id, toUser.id],
+  				data: {
+    				alert: "Your video " + video.get("videoName") + " has been reviewed by " + request.user.get("displayName") + "!"
+  				}
+				}, {
+  				success: function() {
+    				// Push was successful
+  				},
+  				error: function(error) {
+    				// Handle error
+  				}
+			});
+		},
+		error: function(object, error){
+
+		}
+	});
+    
 });
