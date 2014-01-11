7eaf3d1aa71724e8af96a89a0fcf778907d3406f
PhuongNQ <nquangphuong@gmail.com>
2014-01-12 04:49:01 +0900

    post login info to server

--- presentice-web/app.js
+++ presentice-web/app.js
@@ -29,8 +29,8 @@ var helpers = require('express-helpers');
 helpers(app);
 
 // all environments
-//app.set('port', process.env.PORT || 5000);
-app.set('port', 8080);
+app.set('port', process.env.PORT || 5000);
+//app.set('port', 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
