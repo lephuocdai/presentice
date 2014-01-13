c93c7cd32549ff258b3d15601065f696b43654c4
lephuocdai <lephuocdaijapan@gmail.com>
2014-01-13 15:58:16 +0900

    Modify cloudeCode for more elegant push
    Directly use the toUser attribute recently added in Video table

--- cloud-code/cloud/main.js
+++ cloud-code/cloud/main.js
@@ -2,28 +2,19 @@
 Parse.Cloud.afterSave("Review", function(request) {
 	var fromUser = request.object.get("fromUser");
 	var targetVideo = request.object.get("targetVideo");
-	var videoObj = Parse.Object.extend("Video");
-	var query = new Parse.Query(videoObj);
-	query.get(targetVideo.id, {
-		success: function(video){
-			var toUser = video.get("user");
-			Parse.Push.send({
-  				channels: [toUser.id],
-  				data: {
-    				alert: "Your video " + video.get("videoName") + " has been reviewed by " + request.user.get("displayName") + "!",
-    				badge: "Increment"
-  				}
-				}, {
-  				success: function() {
-    				// Push was successful
-  				},
-  				error: function(error) {
-    				// Handle error
-  				}
-			});
-		},
-		error: function(object, error){
-		}
+	var toUser = request.object.get("toUser");
+	Parse.Push.send({
+  		channels: [request.object.get("toUser")],
+  		data: {
+  			alert: "Your video " + video.get("videoName") + " has been reviewed by " + request.user.get("displayName") + "!",
+    		badge: "Increment"
+  		}
+	}, {
+  		success: function() {
+    		// Push was successful
+  		},
+  		error: function(error) {
+    		// Handle error
+  		}
 	});
-    
 });
